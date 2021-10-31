bb71625fbe99f1e6cc55714d33a740c433f26a98
owozsh <owozsh@posteo.net>
2021-10-31 10:03:17 -0300

    add page parser

--- webtex-parser/index.js
+++ webtex-parser/index.js
@@ -2,8 +2,28 @@ import symbols from './symbols.js';
 import readWtex from './readWtex.js';
 
 const webtexDocumentText = readWtex('../example.wtex');
-const webtexDocument = webtexDocumentText.split('\n');
+const webtexDocument = webtexDocumentText.split('\n').filter((e) => e);
 
 let htmlContent = '';
 
-console.log(webtexDocument[1]);
+const parseWebTexDocument = (doc) => {
+  for (let i = 0; i < doc.length; i++) {
+    let line = doc[i];
+    if (line[0] == '.') {
+      parsePage(i, doc);
+    } else if (line[0] == '') {
+      continue;
+    }
+  }
+};
+
+const parsePage = (pageStartLineNumber, doc) => {
+  let page = [doc[pageStartLineNumber]];
+
+  for (let i = pageStartLineNumber + 1; i < doc.length && doc[i] != '.'; i++) {
+    page.push(doc[i]);
+  }
+  console.log(page[page.length - 1]);
+};
+
+parseWebTexDocument(webtexDocument);
